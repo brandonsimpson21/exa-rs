fa2ce85527f14f071616b4f4ea60f4f3526242fa
Brandon Simpson <brandonsimpson21@gmail.com>
2025-10-16 18:57:31 -0500

    Add From `Box<dyn std::Error to ExaApiError

--- src/error.rs
+++ src/error.rs
@@ -9,3 +9,9 @@ pub enum ExaApiError {
     #[error("API error: {0}")]
     ApiError(String),
 }
+
+impl From<Box<dyn std::error::Error>> for ExaApiError {
+    fn from(error: Box<dyn std::error::Error>) -> Self {
+        ExaApiError::ApiError(error.to_string())
+    }
+}
